15d6b1dbcef8c8f0eb4eaa96182b86c5ae5ab1ba
nani454554 <119169444+nani454554@users.noreply.github.com>
2025-11-15 00:40:09 +0530

    Create outputs.tf

---
+++ infrastructure/terraform/modules/rds/outputs.tf
@@ -0,0 +1,35 @@
+output "endpoint" {
+  description = "RDS endpoint"
+  value       = aws_db_instance.main.endpoint
+}
+
+output "address" {
+  description = "RDS address"
+  value       = aws_db_instance.main.address
+}
+
+output "port" {
+  description = "RDS port"
+  value       = aws_db_instance.main.port
+}
+
+output "database_name" {
+  description = "Database name"
+  value       = aws_db_instance.main.db_name
+}
+
+output "master_username" {
+  description = "Master username"
+  value       = aws_db_instance.main.username
+  sensitive   = true
+}
+
+output "security_group_id" {
+  description = "RDS security group ID"
+  value       = aws_security_group.rds.id
+}
+
+output "password_secret_arn" {
+  description = "ARN of Secrets Manager secret containing database password"
+  value       = aws_secretsmanager_secret.db_password.arn
+}
